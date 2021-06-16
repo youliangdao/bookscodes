8093ccbb6dc6b5cc99209261caa54e315d4789dc
youliangdao <youliangdaojing5@gmail.com>
2021-06-16 20:13:01 +0900

    13-6練習

---
+++ chap13/List1306/akagi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#ifndef __AKAGI_C__
+#define __AKAGI_C__
+void akagi(void){
+  printf("akagi!");
+}
+
+#endif
---
+++ chap13/List1306/misaki.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "akagi.c"
+
+int main(void)
+{
+  printf("misaki!");
+  akagi();
+  return 0;
+}
